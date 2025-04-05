10b42f6445d4a36cd00c47f0a3bda213b6a6ecf5
homexhome <homexhome@proton.me>
2025-04-05 14:45:03 +0300

    Tile material

--- assets/test_assets/visibility_range.gd
+++ assets/test_assets/visibility_range.gd
@@ -1,15 +1,8 @@
 extends MeshInstance3D
 
 #func _ready() -> void:
-	#var frame_to_skip : int = randi_range(1,6)
-	#var _frame_to_skip : int = frame_to_skip
-	#while true:
-		#if frame_to_skip > 0:
-			#frame_to_skip -= 1
-			#await get_tree().process_frame
-		#else:
-			#frame_to_skip = _frame_to_skip
-			#update_depth_perceprion()
+	#var mat : BaseMaterial3D = mesh.get_material()
+#
 
 func _physics_process(delta: float) -> void:
 	#if Session.get_depth_perceprion() != visibility_range_end:
