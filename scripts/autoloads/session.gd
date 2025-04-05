b35a38d530892d97c0817716770ee5b72fba655b
homexhome <homexhome@proton.me>
2025-04-06 00:50:00 +0300

    enemy_0

--- scripts/autoloads/session.gd
+++ scripts/autoloads/session.gd
@@ -19,14 +19,18 @@ enum FOG_STATE {OK, STOP}
 var fog : FOG_STATE = FOG_STATE.STOP
 
 func start_fog():
-	fog = FOG_STATE.OK
-	while environment.fog_depth_end > max_depth:
-		environment.fog_depth_end -= 10
+	while environment.environment.fog_depth_end > max_depth:
+		environment.environment.fog_depth_end -= 1
+		if environment.environment.fog_depth_begin > 0:
+			environment.environment.fog_depth_begin = clampf(environment.environment.fog_depth_begin - 0.1, 0.0, 100)
+
+		print(environment.environment.fog_depth_end, " ", environment.environment.fog_depth_begin)
 		await get_tree().process_frame
+	fog = FOG_STATE.OK
+	environment.environment.fog_depth_begin = 0
 
 func stop_fog():
 	fog = FOG_STATE.STOP
-	environment.environment.fog_depth_end = 1000
 
 func pause_player():
 	session_state = STATE.PAUSE
@@ -43,7 +47,8 @@ func flush_session():
 func _physics_process(delta: float) -> void:
 	if fog == FOG_STATE.STOP: return
 	change_depth(-delta * speed_of_depth)
-
+	environment.environment.fog_depth_end = depth_perception
+	
 func get_depth_perceprion() -> float : 
 	return depth_perception
 
--- scripts/game.gd
+++ scripts/game.gd
@@ -11,6 +11,7 @@ func _ready() -> void:
 	start_game()
 
 func start_game():
+	Session.stop_fog()
 	level = level_scene.instantiate()
 	loader_place.add_child(level)
 	
@@ -18,3 +19,8 @@ func start_game():
 	level.add_child(player)
 	player.global_position = level.get_player_spawn()
 	Session.resume_player()
+
+	await get_tree().create_timer(5.0).timeout
+	Session.start_fog()
+	await get_tree().create_timer(1.0).timeout
+	player.give_gun()
